e42438a4532baa9c0e6656e612d3c9d5519dd52f
seejune2 <106861640+seejune2@users.noreply.github.com>
2025-02-05 19:55:43 +0900

    WordQuizScreen ING...

--- app/src/main/java/Learnpackage/WordQuizScreen.kt
+++ app/src/main/java/Learnpackage/WordQuizScreen.kt
@@ -1,21 +1,17 @@
 package Learnpackage
 
-import ProfilePackage.ProfileViewmodel
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -27,9 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.ksj.sauruspang.R
 
@@ -52,6 +46,7 @@ fun WordQuizScreen(avController: NavController, category: String, day: String) {
                             contentDescription = "",
                             modifier = Modifier
                                 .size(50.dp)
+                                .clickable { avController.popBackStack() }
                         )
                         LinearProgressIndicator(
                             progress = { progress },
@@ -96,6 +91,7 @@ fun WordQuizScreen(avController: NavController, category: String, day: String) {
                 modifier = Modifier
                     .size(140.dp)
                     .align(Alignment.CenterEnd)
+                    .clickable { avController.navigate("") }
             )
         }
     }
