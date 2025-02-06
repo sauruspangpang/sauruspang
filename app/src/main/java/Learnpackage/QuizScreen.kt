6095d1e2b47cb0f8e2ab572441d7af33de3b4b55
jordynpark <jordynpark1@gmail.com>
2025-02-06 18:46:29 +0900

    commit pangpangpang

---
+++ app/src/main/java/Learnpackage/QuizScreen.kt
@@ -0,0 +1,167 @@
+package Learnpackage
+
+import ProfilePackage.ProfileViewmodel
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.ksj.sauruspang.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun QuizScreen(
+    navController: NavController,
+    categoryName: String,
+    dayIndex: Int,
+    questionIndex: Int,
+    viewModel: ProfileViewmodel
+) {
+    val category = QuizCategory.allCategories.find { it.name == categoryName }
+    val questions = category?.days?.get(dayIndex)?.questions ?: emptyList()
+    val question = questions[questionIndex]
+    var progress by remember { mutableFloatStateOf(0.2f) } // Example progress (50%)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.arrow),
+                            contentDescription = "button to stagescreen",
+                            modifier = Modifier
+                                .size(50.dp)
+                                .clickable {
+                                    category?.name?.let { categoryName ->
+                                        navController.navigate("stage/$categoryName")
+                                    }
+                                }
+                        )
+
+                        LinearProgressIndicator(
+                            progress = { progress },
+                            modifier = Modifier
+                                .fillMaxWidth(0.5f)
+                                .height(20.dp)
+                                .align(Alignment.Center)
+                        )
+                    }
+                },
+
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFFFDD4AA)
+                )
+            )
+        }
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+                .background(Color(0xFFFDD4AA))
+
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.back),
+                contentDescription = "previous question",
+                modifier = Modifier
+                    .size(140.dp)
+                    .align(Alignment.CenterStart)
+                    .clickable(enabled = questionIndex > 0) {
+                        if (questionIndex > 0) {
+                            navController.navigate("camera/$categoryName/$dayIndex/${questionIndex - 1}")
+                        } else {
+                            navController.popBackStack()
+                        }
+                    }
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(700.dp)
+                    .align(Alignment.Center)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = question.imageId),
+                            contentDescription = "question image",
+                            modifier = Modifier
+                                .size(200.dp)
+
+                        )
+                        Text(
+                            question.korean,
+                            style = TextStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 45.sp
+                            )
+                        )
+
+                    }
+                    Column() {
+                        Button(onClick = {}) { Text("apple") }
+                    }
+
+                }
+
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.frontnull),
+                contentDescription = "next question",
+                modifier = Modifier
+                    .size(140.dp)
+                    .align(Alignment.CenterEnd)
+//                    .clickable(enabled = questionIndex < questions.size - 1)
+//                    {
+//                        navController.navigate("learn/$categoryName/$dayIndex/${questionIndex + 1}") {
+//                            popUpTo("learn/$categoryName/$dayIndex/0") { inclusive = false }
+//                        }
+//
+//                    }
+                    .clickable { navController.navigate("camera/$categoryName/$dayIndex/${questionIndex}") }
+            )
+        }
+    }
+
+}
