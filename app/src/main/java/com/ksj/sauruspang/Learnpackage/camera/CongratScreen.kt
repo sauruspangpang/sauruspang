b9025eb111f3b6fec4087398708309af204c57c7
jordynpark <jordynpark1@gmail.com>
2025-02-11 10:51:49 +0900

    Congrat screen committed

---
+++ app/src/main/java/com/ksj/sauruspang/Learnpackage/camera/CongratScreen.kt
@@ -0,0 +1,11 @@
+package com.ksj.sauruspang.Learnpackage.camera
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.ksj.sauruspang.ProfilePackage.ProfileViewmodel
+
+@Composable
+fun CongratScreen(navController: NavController, viewModel: ProfileViewmodel){
+    Text("Congratulations")
+}
