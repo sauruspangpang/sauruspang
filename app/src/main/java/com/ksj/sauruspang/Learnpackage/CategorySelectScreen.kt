22a8403a74cff5ad6e47627b5306139b46ef1d57
seejune2 <106861640+seejune2@users.noreply.github.com>
2025-02-12 12:57:13 +0900

    profile 별로 이름,프로필 이미지 호출 다르게 적용

--- app/src/main/java/com/ksj/sauruspang/Learnpackage/CategorySelectScreen.kt
+++ app/src/main/java/com/ksj/sauruspang/Learnpackage/CategorySelectScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -206,7 +207,7 @@ fun ProfileBox(scoreViewModel: ScoreViewModel, viewModel: ProfileViewmodel) {
                 Image(
                     painter = painterResource(profile.selectedImage),
                     contentDescription = "",
-                    Modifier.scale(0.8f)
+                    Modifier.scale(0.6f).clip(CircleShape)
                 )
                 Column(
                     modifier = Modifier.padding(bottom = 20.dp),
