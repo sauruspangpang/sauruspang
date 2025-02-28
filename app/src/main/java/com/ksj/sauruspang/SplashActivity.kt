226c9e6458da5966d01256e982a28956075af1e0
Johen In <injunhyun11@gmail.com>
2025-02-28 16:41:56 +0900

    splashscreen 수정 완

--- app/src/main/java/com/ksj/sauruspang/SplashActivity.kt
+++ app/src/main/java/com/ksj/sauruspang/SplashActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -18,6 +19,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -34,13 +36,13 @@ class SplashActivity : ComponentActivity() {
         val controller = WindowInsetsControllerCompat(window, window.decorView)
         controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         controller.hide(WindowInsetsCompat.Type.systemBars())
-        enableEdgeToEdge()
 
         setContent {
             SauruspangTheme {
                 SplashScreen {
                     // 애니메이션 종료 후 MainActivity로 이동
                     startActivity(Intent(this, MainActivity::class.java))
+                    overridePendingTransition(0, 0)
                     finish()
                 }
             }
@@ -50,23 +52,26 @@ class SplashActivity : ComponentActivity() {
 
 @Composable
 fun SplashScreen(onAnimationFinished: () -> Unit) {
-    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.draft_splashscreen))
-    val progress by animateLottieCompositionAsState(
-        composition = composition,
-        iterations = 1 // 애니메이션 1회 재생
-    )
+    Box(modifier = Modifier.fillMaxSize()) {
 
-    // progress 값이 1f 이상이면 애니메이션이 완료된 것으로 판단
-    if (progress >= 1f) {
-        // LaunchedEffect를 사용하여 한 번만 실행되도록 처리
-        LaunchedEffect(key1 = true) {
-            onAnimationFinished()
+        // Lottie 애니메이션
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.renewalsplash))
+        val progress by animateLottieCompositionAsState(
+            composition = composition,
+            iterations = 1 // 애니메이션 1회 재생
+        )
+
+        if (progress >= 1f) {
+            LaunchedEffect(key1 = true) {
+                onAnimationFinished()
+            }
         }
-    }
 
-    LottieAnimation(
-        composition = composition,
-        progress = progress,
-        modifier = Modifier.fillMaxSize()
-    )
+        LottieAnimation(
+            composition = composition,
+            progress = progress,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
+        )
+    }
 }
